6053b3e7ed9c0f19359726c2aec79663c4690165
tttc6 <38695895+tttc6@users.noreply.github.com>
2025-09-26 20:27:16 +0100

    built out task component

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,16 +1,17 @@
 import type { StorybookConfig } from "@storybook/react-vite";
 
 const config: StorybookConfig = {
-  stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
+  stories: ["../src/components/**/*.stories.@(ts|tsx)"],
   staticDirs: ["../public"],
   addons: [
     "@storybook/addon-docs",
     "@storybook/addon-vitest",
-    "@chromatic-com/storybook"
+    "@chromatic-com/storybook",
   ],
   framework: {
     name: "@storybook/react-vite",
     options: {},
   },
 };
+
 export default config;
